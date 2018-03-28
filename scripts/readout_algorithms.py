e42cdad5cf79b1d370b85c4be38bc7198223424b
Nathaniel Odell <naodell@bahamut.phys.northwestern.edu>
2018-03-28 17:38:36 -0500

    splitting event mixing and algorithms

--- scripts/readout_algorithms.py
+++ scripts/readout_algorithms.py
@@ -1,50 +1,50 @@
 
+from functools import partial
 import numpy as np
 import pandas as pd
 
-def threshold_algos(df, sort_by, name, ascending=False, nbx=8):
+def threshold_algo(df, name, scan_vals, n_readout = 10, sort_by='reco_e', ascending=False):
+    mask = np.zeros((df.shape[0], scan_vals.size))
+    mask[:n_readout] = True
+
+    cols = [f'{name}_{c}' for c in scan_vals]
     df = df.sort_values(sort_by, ascending=ascending)
-    df.loc[:,name] = True
-    if df.shape[0] > nbx*10:
-        df.loc[nbx*10:,name] = False
-
+    df = df.reset_index(drop=True)
+    df.loc[:, cols] = np.logical_and(df.loc[:, cols], mask)
 
     return df
 
-def algos_8bx(df):
+def algorithm_test(df, mippt_scan):
 
-    # 8bx no sort
-    df = df.reset_index(drop=True) # causes problems otherwise
-    df = threshold_algos(df, 
-                         sort_by=['ievt', 'cell'], 
-                         name='threshold_8bx_nosort',
-                         ascending=True, 
-                         nbx=8
-                         )
-    # 8bx energy sort
-    df = threshold_algos(df, 
-                         sort_by='reco_e', 
-                         name='threshold_8bx_esort',
-                         ascending=False, 
-                         nbx=8
-                         )
+    # 1 bx algorithms
+    func = partial(threshold_algo, 
+                   name = 'threshold_1bx_nosort', 
+                   scan_vals = mippt_scan, 
+                   n_readout=10, 
+                   sort_by=['ievt', 'cell'], 
+                   ascending=True
+                   )
+    df = df.groupby('ievt').apply(func)
+    
+    func = partial(threshold_algo, 
+                   name = 'threshold_1bx_esort', 
+                   scan_vals = mippt_scan, 
+                   n_readout=10, 
+                   sort_by='reco_e', 
+                   ascending=False
+                   )
+    df = df.groupby('ievt').apply(func)
+
+    # 8 bx algorithms
+    df = threshold_algo(df, 'threshold_8bx_nosort',  mippt_scan,
+                        n_readout = 80, 
+                        sort_by=['ievt', 'cell'], 
+                        ascending=True
+                        )
+    df = threshold_algo(df, 'threshold_8bx_esort', mippt_scan,
+                        n_readout = 80, 
+                        sort_by='reco_e', 
+                        ascending=False
+                        )
     return df
 
-def algos_1bx(df):
-
-    # 8bx no sort
-    df = df.reset_index(drop=True) # causes problems otherwise
-    df = threshold_algos(df, 
-                         sort_by=['ievt', 'cell'], 
-                         name='threshold_1bx_nosort',
-                         ascending=True, 
-                         nbx=1
-                         )
-         # 8bx energy sort
-    df = threshold_algos(df, 
-                         sort_by='reco_e', 
-                         name='threshold_1bx_esort',
-                         ascending=False, 
-                         nbx=1
-                         )
-    return df
