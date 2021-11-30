360380342e87c49bfb6b8c6f6af47cdfb8aba613
Nathaniel Jay Odell <naodell@gmail.com>
2021-11-30 15:03:14 -0600

    Implemented convolutional auotencoder

--- scripts/nn_helpers.py
+++ scripts/nn_helpers.py
@@ -1,5 +1,4 @@
 
-
 import numpy as np
 import pandas as pd
 
@@ -31,30 +30,37 @@ class MyDataset(Dataset):
 class AutoEncoder(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(AutoEncoder, self).__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(input_dim, 512),
+        self.conv2d_1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)
+        self.act_1    = nn.Relu()
+        self.pool_1   = nn.MaxPool2d(2)
+        self.conv2d_2 = nn.Conv2d(1, 8, kernel_size=2, padding=0, stride=2)
+        self.act_2    = nn.Relu()
+        self.pool_2   = nn.MaxPool2d(2)
+        pooled_dim    = input_dim # n_conv_output / pooling_size
+        self.encoder = nn.Sequential(
+            nn.Linear(pooled_dim, 128),
             nn.ReLU(),
-            nn.Linear(512, 256),
+            nn.Linear(128, 64),
             nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            #nn.Linear(128, 64),
-            #nn.ReLU(),
-            #nn.Linear(64, 128),
-            #nn.ReLU(),
+            nn.Linear(, 64),
+            nn.ReLU()
+            )
+        self.decoder = nn.Sequential(
             nn.Linear(128, 256),
             nn.ReLU(),
-            nn.Linear(256, 512),
-            nn.ReLU(),
+            #nn.Linear(256, 512),
+            #nn.ReLU(),
             nn.Linear(512, output_dim),
             nn.ReLU()
         )
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        #x = nn.Sigmoid(logits)
-        x = logits
+        x = x.unsqueeze(1) # adds channel dimension for the case that there is one channel per image
+        x = self.conv2d(x)
+        x = self.act1(x)
+        x = self.pool1(x)
+        x = x.view(-1, 8 * 8 * 8)
+        x = self.encoder(x)
+        x = self.decoder(x)
         return x
 
