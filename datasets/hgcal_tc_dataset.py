0a5b59811e06c2a06ddccbb4207ed5518bfeafcb
Nathaniel Jay Odell <naodell@gmail.com>
2022-03-04 14:20:39 -0600

    First version of wafer data for autoencoder training

--- datasets/hgcal_tc_dataset.py
+++ datasets/hgcal_tc_dataset.py
@@ -1,77 +1,39 @@
 
 import torch
-from torch import Dataset
+from torch.utils.data import Dataset
 
-class HGCalTCModuleData(Dataset):
+class HGCalTCModuleDataset(Dataset):
     '''
-    Data format for individual wafers using (cellu, cellv) formatting.
+    Data format for individual wafers using (cellu, cellv) formatting.  This is
+    the first prototype of the econ dataset and will not include layer of wafer
+    location information.
     '''
     def __init__(self, input_files, targets, transform=None):
         self.input_files = input_files
         self.transform   = transform
 
+        # unpack the data into 8x8 tensors
+        self.wafers = unpack_wafers()
+
     def __getitem__(self, index):
         x = self.data[index]
-        y = self.targets[index]
+        #y = self.targets[index]
 
         if self.transform:
             x = self.transform(x)
 
-        return x, y
+        return x#, y
 
     def __len__(self):
         return len(self.data)
 
-    def blacklist(self, npzs):
-        """
-        Remove a list of npzs from the dataset
-        Useful to remove bad events
-        """
-        for npz in npzs: self.npzs.remove(npz)
+    def unpack_wafer_data(self):
+        wafers = []
+        for filename in self.input_files:
+            f = open(filename, 'rb')
+            data = pickle.load(f)
+            for (event, zside), event_data in data.items():
+                for (waferu, waferv), tc_stack in event_data.items():
+                    wafers.append(tc_stack)
 
-    def get(self, i):
-        d = np.load(self.npzs[i])
-        x = d['recHitFeatures']
-        if self.flip and np.mean(x[:,7]) < 0:
-            # Negative endcap: Flip z-dependent coordinates
-            x[:,1] *= -1 # eta
-            x[:,7] *= -1 # z
-
-        cluster_index = incremental_cluster_index_np(d['recHitTruthClusterIdx'].squeeze())
-        if np.all(cluster_index == 0):
-            print('WARNING: No objects in', self.npzs[i])
-
-        truth_cluster_props = np.hstack((
-            d['recHitTruthEnergy'],
-            d['recHitTruthPosition'],
-            d['recHitTruthTime'],
-            d['recHitTruthID'],
-            ))
-        assert truth_cluster_props.shape == (x.shape[0], 5)
-        order = cluster_index.argsort()
-
-        return Data(
-            x = torch.from_numpy(x[order]).type(torch.float),
-            y = torch.from_numpy(cluster_index[order]).type(torch.int),
-            truth_cluster_props = torch.from_numpy(truth_cluster_props[order]).type(torch.float),
-            inpz = torch.Tensor([i])
-            )
-
-    def __len__(self):
-        return len(self.npzs)
-
-    def len(self):
-        return len(self.npzs)
-
-    def split(self, fraction):
-        """
-        Creates two new instances of TauDataset with a fraction of events split
-        """
-        left = self.__class__(self.root)
-        right = self.__class__(self.root)
-        split_index = int(fraction*len(self))
-        left.npzs = self.npzs[:split_index]
-        right.npzs = self.npzs[split_index:]
-
-        return left, right
-
+        self.data = np.vstack(wafers)
